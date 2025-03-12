d5fec36126a69f14fa03001583c23ac21b098273
latrisha <l_gomer@yahoo.com>
2025-03-12 11:43:56 -0700

    wip: define and export pass -user-to-view

---
+++ middleware/pass-user-to-view.js
@@ -0,0 +1,16 @@
+const passUserToView = (req, res, next) => {
+    res.locals.user = req.session.user ? req.session.user : null;
+    //anything we need to access in our templates Globally
+    //can be added as a property to res.locals
+    //res is short for response
+    //generating templates is part of the response
+    next(); //call the next piece of middleware
+}
+
+///this is how to write the same thing as an if/else statement
+// if(req.session.user) {
+//     res.locals.user = req.session.user { 
+//     } else {
+//         res.locals.user = null
+//     }
+// }
